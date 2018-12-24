a34c5780b9f866f0ea9319494cd99f045f601b9f
Clem-Fern-Dev <clement.coussieu@gmail.com>
2018-12-24 01:24:54 +0100

    Update import in MainPlugin class

--- src/main/java/fr/nodropingitems/MainPlugin.java
+++ src/main/java/fr/nodropingitems/MainPlugin.java
@@ -1,5 +1,11 @@
 package fr.nodropingitems;
 
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerDropItemEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class MainPlugin extends JavaPlugin {
