04567264c33201147134f2fc369945a6c5f998a4
Clem-Fern-Dev <clement.coussieu@gmail.com>
2018-12-24 01:25:23 +0100

    Add forbidden event

--- src/main/java/fr/nodropingitems/MainPlugin.java
+++ src/main/java/fr/nodropingitems/MainPlugin.java
@@ -13,8 +13,40 @@ public class MainPlugin extends JavaPlugin implements Listener {
 	@Override
 	public void onEnable() {
 		
-		
+		getServer().getPluginManager().registerEvents(this, this);
 		
 	}
+	
+	@EventHandler
+	public void onPlayerDropEvent(PlayerDropItemEvent event) {
+		
+		Player player = event.getPlayer();
+		
+		if(!player.hasPermission("nodropingitems.bypass")) {
+			
+			event.setCancelled(true);
+			
+		}
+		
+	}
+	
+	@EventHandler
+	public void onPlayerLaunchSlapshPotion(PlayerInteractEvent event) {
+		
+		Player player = event.getPlayer();
+		Material material = event.getMaterial();
+		
+		if(material == null)
+			return;
+		
+		if(material.equals(Material.SPLASH_POTION) && !player.hasPermission("nodropingitems.bypass")) {
+			
+			event.setCancelled(true);
+			
+		}
+		
+	}
+	
+	
 	
 }
